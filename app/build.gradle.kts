ccbb63245e6eb6d6406a26581d75e2f81f27448d
tinnamchoi <tinnam.choi@gmail.com>
2026-01-23 22:33:47 +1100

    chore: bump version to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.tinnamchoi.tmtlauncher"
         minSdk = 21
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
